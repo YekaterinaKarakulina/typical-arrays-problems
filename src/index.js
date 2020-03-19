51887ed1d23b3a6e1c96e5b804914f734ae29472
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-19 19:57:26 +0600

    feat: typical-array-problems task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  console.log(typeof(array));
+  if(typeof(array) =='undefined'|| array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  console.log(typeof(array));
+  if(typeof(array) =='undefined' || array.length == 0) {
+    return 0;
+  } else {
+  return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  console.log(typeof(array));
+  if(typeof(array) =='undefined' || array.length == 0) {
+    return 0;
+  } else {
+    var sum = 0;
+    for(var i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+  }
 }
